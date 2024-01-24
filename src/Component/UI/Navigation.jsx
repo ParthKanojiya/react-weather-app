8d431bd996d4933e44c6b3942bdf45a6f2751c16
parthkanojiya <gajjarparth5055@gmail.com>
2024-01-24 22:14:04 +0530

    dark mode fixed

--- src/Component/UI/Navigation.jsx
+++ src/Component/UI/Navigation.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { RxMagnifyingGlass, RxSun } from "react-icons/rx";
 import { FaLocationDot } from "react-icons/fa6";
-import WeatherCard from './WeatherCard';
-
+import WeatherCard from "./WeatherCard";
 
 const Navigation = () => {
     const [searchValue, setSearchValue] = useState("");
@@ -12,9 +11,10 @@ const Navigation = () => {
     // Default location value
     const defaultLocation = "ahmedabad";
 
-    let API_KEY = "b1e7c4cb104550c0c693ceb415ac3553"
+    let API_KEY = "b1e7c4cb104550c0c693ceb415ac3553";
 
-    let URL = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue || defaultLocation}&appid=${API_KEY}&units=metric`;
+    let URL = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue || defaultLocation
+        }&appid=${API_KEY}&units=metric`;
 
     const fetchWeatherInfo = async () => {
         try {
@@ -36,67 +36,67 @@ const Navigation = () => {
                 icon: data.weather[0].icon,
             };
             setTempInfo(myWeatherInfo);
-
         } catch (error) {
-            console.log('Error fetching weather info:', error);
+            console.log("Error fetching weather info:", error);
         }
-    }
+    };
 
     useEffect(() => {
-        const fetchDataAndSetTheme = async () => {
-            await fetchWeatherInfo();
+        fetchWeatherInfo();
+        const theme = localStorage.getItem("theme");
+        if (theme == "dark") setDarkMode(false);
+    }, []);
 
-            const theme = localStorage.getItem("theme");
-            if (theme === "dark") {
-                setDarkMode(false);
-                document.documentElement.classList.add('dark');
-            } else {
-                document.documentElement.classList.remove('dark');
-                localStorage.setItem("theme", "light");
-            }
-        };
-
-        fetchDataAndSetTheme();
+    useEffect(() => {
+        if (darkMode) {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+        }
     }, [darkMode]);
 
-
     return (
-
         <>
             <nav className="flex flex-col-reverse md:flex-row w-full justify-between py-4 gap-4">
-
                 <div className="location flex items-center gap-2">
-                    <FaLocationDot className='text-2xl' />
-                    <h2 className='font-bold text-sm md:text-2xl'>{tempInfo?.name}, {tempInfo?.country}</h2>
+                    <FaLocationDot className="text-2xl" />
+                    <h2 className="font-bold text-sm md:text-2xl">
+                        {tempInfo?.name}, {tempInfo?.country}
+                    </h2>
                 </div>
 
                 <div className="flex w-full items-center gap-4 sm:w-fit">
-
                     <button className="inline-flex items-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent/50 hover:text-accent-foreground rounded-md h-9 w-full max-w-80 md:max-w-screen-sm lg:max-w-screen-lg lg:w-96 whitespace-nowrap px-4">
                         <p className="flex items-center justify-between gap-4 text-sm text-muted-foreground w-full h-full">
-
-                            <input type="search" placeholder='Search city...' className='w-full h-full outline-none text-base dark:bg-black dark:text-white'
+                            <input
+                                type="search"
+                                placeholder="Search city..."
+                                className="w-full h-full outline-none text-base dark:bg-dark dark:text-white"
                                 value={searchValue}
                                 onChange={(e) => setSearchValue(e.target.value)}
                             />
-                            <RxMagnifyingGlass className='text-xl font-bold'
+                            <RxMagnifyingGlass
+                                className="text-xl font-bold"
                                 onClick={fetchWeatherInfo}
                             />
                         </p>
                     </button>
 
-                    <button className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent/50 hover:text-accent-foreground w-10 h-9 shrink-0" type="button"
+                    <button
+                        className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent/50 hover:text-accent-foreground w-10 h-9 shrink-0"
+                        type="button"
                         onClick={() => setDarkMode(!darkMode)}
                     >
-                        <RxSun className='text-xl' />
+                        <RxSun className="text-xl" />
                     </button>
                 </div>
             </nav>
 
             <WeatherCard tempInfo={tempInfo} />
         </>
+    );
+};
 
-    )
-}
-
-export default Navigation
+export default Navigation;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  // darkMode: "class",
   theme: {
     extend: {
       colors: {
         dark: "#292929",
       },
+      backgroundColor: {
+        dark: "#001122",
+      },
     },
   },
+  darkMode: "class",
   plugins: [],
 };
