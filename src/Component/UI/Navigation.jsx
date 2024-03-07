37567200a10d36846eb892fc2f89ec520b7f7f3b
parthkanojiya <gajjarparth5055@gmail.com>
2024-03-07 19:39:10 +0530

    Shimmer UI Added

--- src/Component/UI/Navigation.jsx
+++ src/Component/UI/Navigation.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { RxMagnifyingGlass, RxSun } from "react-icons/rx";
 import { FaLocationDot } from "react-icons/fa6";
 import WeatherCard from "./WeatherCard";
+import Shimmer from "./Shimmer";
 
 const Navigation = () => {
   const [searchValue, setSearchValue] = useState("");
@@ -60,42 +61,48 @@ const Navigation = () => {
 
   return (
     <>
-      <nav className="flex flex-col-reverse md:flex-row w-full justify-between py-4 gap-4">
-        <div className="location flex items-center gap-2">
-          <FaLocationDot className="text-2xl" />
-          <h2 className="text-lg md:text-2xl font-inter font-extrabold">
-            {tempInfo?.name}, {tempInfo?.country}
-          </h2>
-        </div>
+      {tempInfo.length === 0 ? (
+        <Shimmer />
+      ) : (
+        <>
+          <nav className="flex flex-col-reverse md:flex-row w-full justify-between py-4 gap-4">
+            <div className="location flex items-center gap-2">
+              <FaLocationDot className="text-2xl" />
+              <h2 className="text-lg md:text-2xl font-inter font-extrabold">
+                {tempInfo?.name}, {tempInfo?.country}
+              </h2>
+            </div>
 
-        <div className="flex w-full items-center gap-4 sm:w-fit">
-          <button className="inline-flex items-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent/50 hover:text-accent-foreground rounded-md w-full max-w-80 md:max-w-screen-sm lg:max-w-screen-lg lg:w-96 whitespace-nowrap px-4 dark:border-slate-700 dark:shadow-none bg-border h-12">
-            <p className="flex items-center justify-between gap-4 text-sm text-muted-foreground w-full h-full">
-              <input
-                type="search"
-                placeholder="Search city..."
-                className="w-full h-full outline-none text-base dark:bg-dark dark:text-white bg-transparent dark:shadow-none font-inter font-medium"
-                value={searchValue}
-                onChange={(e) => setSearchValue(e.target.value)}
-              />
-              <RxMagnifyingGlass
-                className="text-xl font-bold"
-                onClick={fetchWeatherInfo}
-              />
-            </p>
-          </button>
+            <div className="flex w-full items-center gap-4 sm:w-fit">
+              <button className="inline-flex items-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent/50 hover:text-accent-foreground rounded-md w-full max-w-80 md:max-w-screen-sm lg:max-w-screen-lg lg:w-96 whitespace-nowrap px-4 dark:border-slate-700 dark:shadow-none bg-border h-12">
+                <p className="flex items-center justify-between gap-4 text-sm text-muted-foreground w-full h-full">
+                  <input
+                    type="search"
+                    placeholder="Search city..."
+                    className="w-full h-full outline-none text-base dark:bg-dark dark:text-white bg-transparent dark:shadow-none font-inter font-medium"
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                  />
+                  <RxMagnifyingGlass
+                    className="text-xl font-bold"
+                    onClick={fetchWeatherInfo}
+                  />
+                </p>
+              </button>
 
-          <button
-            className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent/50 hover:text-accent-foreground w-12 shrink-0 bg-border dark:shadow-none dark:border-slate-700 h-12"
-            type="button"
-            onClick={() => setDarkMode(!darkMode)}
-          >
-            <RxSun className="text-xl" />
-          </button>
-        </div>
-      </nav>
+              <button
+                className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent/50 hover:text-accent-foreground w-12 shrink-0 bg-border dark:shadow-none dark:border-slate-700 h-12"
+                type="button"
+                onClick={() => setDarkMode(!darkMode)}
+              >
+                <RxSun className="text-xl" />
+              </button>
+            </div>
+          </nav>
 
-      <WeatherCard tempInfo={tempInfo} />
+          <WeatherCard tempInfo={tempInfo} />
+        </>
+      )}
     </>
   );
 };
--- src/Component/UI/Shimmer.jsx
+++ src/Component/UI/Shimmer.jsx
@@ -1,74 +1,91 @@
-import React from 'react'
+import React from "react";
 
 const Shimmer = () => {
-    return (
-        <section className="bg-white dark:bg-gray-900">
-            <div className="container px-6 py-10 mx-auto animate-pulse">
-                <h1 className="w-48 h-2 mx-auto bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+  return (
+    <section className="bg-white dark:bg-gray-900 h-dvh">
+      <div className="container mx-auto animate-pulse">
+        {/* Nav Shimmer Start */}
+        <div className="flex items-center justify-between py-4 w-full gap-4">
+          <h1 className="w-80 h-12 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+          <div className="w-96 h-12 bg-gray-200 rounded-lg dark:bg-gray-700"></div>
+        </div>
+        {/* Nav Shimmer End */}
 
-                <p className="w-64 h-2 mx-auto mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                <p className="w-64 h-2 mx-auto mt-4 bg-gray-200 rounded-lg sm:w-80 dark:bg-gray-700"></p>
-
-                <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
-                    <div className="w-full ">
-                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-
-                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                    </div>
-
-                    <div className="w-full ">
-                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-
-                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                    </div>
-
-                    <div className="w-full ">
-                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-
-                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                    </div>
-
-                    <div className="w-full ">
-                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-
-                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                    </div>
-
-                    <div className="w-full ">
-                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-
-                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                    </div>
-
-                    <div className="w-full ">
-                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-
-                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                    </div>
-
-                    <div className="w-full ">
-                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-
-                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                    </div>
-
-                    <div className="w-full ">
-                        <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
-
-                        <h1 className="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                        <p className="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                    </div>
-                </div>
+        <div className="grid grid-cols-1 grid-rows-3 h-full gap-4 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {/* Card 1 */}
+          <div className="w-full row-span-2">
+            <div className="flex flex-col justify-between p-6 w-full h-full bg-gray-300 rounded-lg dark:bg-gray-600">
+              <div className="flex flex-col gap-8">
+                <p className="w-24 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+                <p className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+              </div>
+              <div className="flex items-center justify-center">
+                <p className="w-48 h-32 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+              </div>
+              <div className="flex flex-col gap-8">
+                <p className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+                <p className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+              </div>
             </div>
-        </section>
-    )
-}
+          </div>
 
-export default Shimmer
+          {/* Card 2 */}
+          <div className="w-full ">
+            <div className="flex flex-col justify-between p-6 w-full h-56 bg-gray-300 rounded-lg dark:bg-gray-600">
+              <h1 className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></h1>
+              <p className="w-24 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+              <p className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+            </div>
+          </div>
+
+          {/* Card 3 */}
+          <div className="w-full ">
+            <div className="flex flex-col justify-between p-6 w-full h-56 bg-gray-300 rounded-lg dark:bg-gray-600">
+              <h1 className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></h1>
+              <p className="w-24 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+              <p className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+            </div>
+          </div>
+
+          {/* Card 4 */}
+          <div className="w-full ">
+            <div className="flex flex-col justify-between p-6 w-full h-56 bg-gray-300 rounded-lg dark:bg-gray-600">
+              <h1 className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></h1>
+              <p className="w-24 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+              <p className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+            </div>
+          </div>
+
+          {/* Card 5 */}
+          <div className="w-full ">
+            <div className="flex flex-col justify-between p-6 w-full h-56 bg-gray-300 rounded-lg dark:bg-gray-600">
+              <h1 className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></h1>
+              <p className="w-24 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+              <p className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+            </div>
+          </div>
+
+          {/* Card 6 */}
+          <div className="w-full ">
+            <div className="flex flex-col justify-between p-6 w-full h-56 bg-gray-300 rounded-lg dark:bg-gray-600">
+              <h1 className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></h1>
+              <p className="w-24 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+              <p className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+            </div>
+          </div>
+
+          {/* Card 7 */}
+          <div className="w-full ">
+            <div className="flex flex-col justify-between p-6 w-full h-56 bg-gray-300 rounded-lg dark:bg-gray-600">
+              <h1 className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></h1>
+              <p className="w-24 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+              <p className="w-56 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Shimmer;
